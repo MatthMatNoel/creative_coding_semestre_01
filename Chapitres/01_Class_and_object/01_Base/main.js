e5c80d7e3a871c01ef39083aa9a0db2ff1a73a10
MatthMatNoel <matthieu.noel@ecal.ch>
2024-09-27 11:22:29 +0200

    Class and object lesson

---
+++ Chapitres/01_Class_and_object/01_Base/main.js
@@ -0,0 +1,9 @@
+// Import the App class
+import App from "./js/App.js";
+
+// Create an instance of the App class
+let app = new App();
+
+// Create a canvas and draw a circle
+app.createCanvas(400, 400);
+app.circle(200, 200, 100);
